cefeff0269f24948de62e199b284e6d7ef243ecf
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-10-06 22:11:04 +0200

    create catalog and catalog_graphql apps

--- buython_project/settings.py
+++ buython_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'catalog.apps.CatalogConfig',
+    'catalog_graphql.apps.CatalogGraphqlConfig'
 ]
 
 MIDDLEWARE = [
